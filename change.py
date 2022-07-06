e58ebd02077bdb70b75c369bfb2e1010042f532f
Иван <sigaga511@gmail.com>
2022-07-06 16:39:35 +0300

    Initial commit

---
+++ change.py
@@ -0,0 +1,4 @@
+my_list=[3,5,6,7,8,9,15,7]
+
+for x in my_list:
+    print('{}^2={}'.format(x,x**2))
---
+++ del.py
@@ -0,0 +1,7 @@
+my_list=[1,2,3,4,5,6,7,8]
+
+print(my_list)
+
+del my_list=[7,9]
+
+print(my_list)
